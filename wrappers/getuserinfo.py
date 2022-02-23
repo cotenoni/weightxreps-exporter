b5923df661db51162d9f90f24445bfa3282b6e84
Nicolas Cote-Nolin <nicolas.cotenolin@gmail.com>
2022-02-22 19:53:32 -0500

    Removed unused override of get method in getuserinfo wrapper.

--- wrappers/getuserinfo.py
+++ wrappers/getuserinfo.py
@@ -4,8 +4,7 @@ class GetUserInfo(BaseRequestWrapper):
     """
     Wrapper for the GetUserInfo GraphQL query.
 
-    This query allows us to get all workouts ending at a date (to) for a given range of 3, 6, 8, 12, 16 weeks back. 
-    For example, a "to" date of 2021-10-04 and a range of 3, will give us all workouts from 2021-09-13 to 2021-10-04.
+    This query allows us to get all the user informations based on it's username. 
     """
     query = {
         "operationName": "GetUserInfo", 
@@ -21,12 +20,6 @@ class GetUserInfo(BaseRequestWrapper):
 
         self.username = username
         self.query["variables"]["userInfoUname"] = self.username
-        
-    def get(self):
-        # TODO multiple queries necessary here because of the range variable limitation
-        # loop and modify the query variables
-        # check schema first to make sure there's not other way
-        super().get()
 
     def parse(self):
         self.user_id = self.data['userInfo']["user"]["id"]
